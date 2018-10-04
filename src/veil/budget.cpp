fa302380d7b41581905f76d3616dc58610e9b6d5
Gabriel <gabrielrobinson0025@gmail.com>
2018-10-04 16:16:15 -0600

    Once monthly rewards (#14)
    * Modified rewards based upon block size. Still need to make it so that rewards are generated after so many blocks and then test.

--- src/veil/budget.cpp
+++ src/veil/budget.cpp
@@ -24,6 +24,68 @@ bool CheckBudgetTransaction(const CTransaction& tx, CValidationState& state)
     return true;
 }
 
+/**
+ * Rewards are generated once a month. If not the right block height then the rewards are set to 0.
+ * Rewards are based upon the height of the block.
+ * @param nBlockHeight
+ */
+void BudgetParams::GetBlockRewards(int nBlockHeight, CAmount& nBlockReward,
+        CAmount& nFounderPayment, CAmount& nLabPayment, CAmount& nBudgetPayment)
+{
+
+    if (nBlockHeight <= 0 || (nBlockHeight % 43830) != 0) { // 43830 is the average size of a month in minutes when including leap years
+                                                            // 44640 is the length of a month when not including leap years
+        nBlockReward = 0;
+        nFounderPayment = 0;
+        nLabPayment = 0;
+        nBudgetPayment = 0;
+
+    } else if (nBlockHeight >= 1 && nBlockHeight <= 381599) {
+
+        nBlockReward = 50 * 43830;
+        nFounderPayment = 10 * 43830;
+        nLabPayment = 10 * 43830;
+        nBudgetPayment = 30 * 43830;
+
+    } else if (nBlockHeight >= 381600 && nBlockHeight <= 763199) {
+
+        nBlockReward = 40 * 43830;
+        nFounderPayment = 8 * 43830;
+        nLabPayment = 8 * 43830;
+        nBudgetPayment = 24 * 43830;
+    } else if (nBlockHeight >= 763200 && nBlockHeight <= 1144799) {
+
+        nBlockReward = 30 * 43830;
+        nFounderPayment = 6 * 43830;
+        nLabPayment = 6 * 43830;
+        nBudgetPayment = 18 * 43830;
+
+    } else if (nBlockHeight >= 1144800 && nBlockHeight <= 1526399) {
+
+        nBlockReward = 20 * 43830;
+        nFounderPayment = 4 * 43830;
+        nLabPayment = 4 * 43830;
+        nBudgetPayment = 12 * 43830;
+
+    } else if (nBlockHeight >= 1526400 && nBlockHeight <= 1907999) {
+
+        nBlockReward = 10 * 43830;
+        nFounderPayment = 2 * 43830;
+        nLabPayment = 2 * 43830;
+        nBudgetPayment = 6 * 43830;
+
+    } else {
+
+        nBlockReward = 10 * 43830;
+        nFounderPayment = 0 * 43830;
+        nLabPayment = 2 * 43830;
+        nBudgetPayment = 8 * 43830;
+
+    }
+
+
+}
+
 BudgetParams::BudgetParams(std::string strNetwork)
 {
     if (strNetwork == "main") {
@@ -44,6 +106,7 @@ BudgetParams* BudgetParams::Get()
     static BudgetParams instance(strNetwork);
     return &instance;
 }
+
 
 BudgetParams& Budget()
 {
