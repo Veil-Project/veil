e2ce805c62da72a269172cadac76154d21e90a17
Nithin Chalapathi <nithin.ch10@gmail.com>
2018-10-04 10:26:25 -0600

    Add 10,000 denomination to zerocoin and remove old denomination enums.

--- src/libzerocoin/Denominations.h
+++ src/libzerocoin/Denominations.h
@@ -13,18 +13,14 @@ namespace libzerocoin {
 
 enum  CoinDenomination {
     ZQ_ERROR = 0,
-    ZQ_ONE = 1,
-    ZQ_FIVE = 5,
     ZQ_TEN = 10,
-    ZQ_FIFTY = 50,
     ZQ_ONE_HUNDRED = 100,
-    ZQ_FIVE_HUNDRED = 500,
     ZQ_ONE_THOUSAND = 1000,
-    ZQ_FIVE_THOUSAND = 5000
+    ZQ_TEN_THOUSAND = 10000
 };
 
 // Order is with the Smallest Denomination first and is important for a particular routine that this order is maintained
-const std::vector<CoinDenomination> zerocoinDenomList = {ZQ_TEN, ZQ_ONE_HUNDRED, ZQ_ONE_THOUSAND};
+const std::vector<CoinDenomination> zerocoinDenomList = {ZQ_TEN, ZQ_ONE_HUNDRED, ZQ_ONE_THOUSAND, ZQ_TEN_THOUSAND};
 // These are the max number you'd need at any one Denomination before moving to the higher denomination. Last number is 4, since it's the max number of
 // possible spends at the moment    /
 const std::vector<int> maxCoinsAtDenom   = {4, 1, 4, 1, 4, 1, 4, 4};
