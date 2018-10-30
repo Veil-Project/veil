04c9c6106291e69730c634591c73831df713f255
Oilplik <36316122+Oilplik@users.noreply.github.com>
2018-10-30 16:42:21 -0600

    OSx Build
    Added CI files for the OSx build

---
+++ contrib/bamboo/ci-mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+mkdir `pwd`/depends/SDKs
+tar -C `pwd`/depends/SDKs/ -xf /SDKs/MacOSX10.11.sdk.tar.gz
+cd depends/
+echo `pwd`
+make HOST=x86_64-apple-darwin11
+cd ../
+./autogen.sh
+./configure --prefix=`pwd`/depends/x86_64-apple-darwin11
+make
+echo "done"
