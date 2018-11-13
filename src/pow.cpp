3133d68548f6bb04c9660fb46a12a982c138dd86
presstab <presstab1337@gmail.com>
2018-11-12 21:16:59 -0700

    Change X16r to use timestamp as entropy instead of prev block

--- src/pow.cpp
+++ src/pow.cpp
@@ -101,10 +101,5 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     }
 
     // Check proof of work matches claimed amount
-    if (UintToArith256(hash) > bnTarget) {
-        //std::cout << (UintToArith256(hash) > bnTarget) << "\n";
-        return false;
-    }
-
-    return true;
+    return UintToArith256(hash) < bnTarget;
 }
