595e106bc81d40e2e89b7948df17958b40d78c20
4x13 <39984029+4x13@users.noreply.github.com>
2018-12-20 13:06:00 -0800

    Update veil.cpp for search replace error

--- src/qt/veil.cpp
+++ src/qt/veil.cpp
@@ -565,7 +565,7 @@ int main(int argc, char *argv[])
     // Do not refer to data directory yet, this can be overridden by Intro::pickDataDirectory
 
     /// 1. Basic Qt initialization (not dependent on parameters or configuration)
-    Q_INIT_RESOURCE(veil);
+    Q_INIT_RESOURCE(bitcoin);
     Q_INIT_RESOURCE(bitcoin_locale);
 
     BitcoinApplication app(*node, argc, argv);
