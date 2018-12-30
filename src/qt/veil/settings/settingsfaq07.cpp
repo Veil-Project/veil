2d254178f5c95fd9455aaa89c1eb3e0bd6bfcaab
Strontium <38264865+StrontiumZ38@users.noreply.github.com>
2018-12-30 14:13:10 +0900

    Update settingsfaq07.cpp

--- src/qt/veil/settings/settingsfaq07.cpp
+++ src/qt/veil/settings/settingsfaq07.cpp
@@ -7,13 +7,13 @@ SettingsFaq07::SettingsFaq07(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->label_2->setText("VEIL wallets can automatically generate receiving addresses and QR codes which contain the receiving addresses.<br><br>"
+    ui->label_2->setText("Veil wallets can automatically generate receiving addresses and QR codes which contain the receiving addresses.<br><br>"
 
-                         "You can share your QR code with others so they can send VEIL to you, and others can send their QR code to you so you can send VEIL to them.<br><br>"
+                         "You can share your QR code with others so they can send veil to you, and others can send their QR code to you so you can send veil to them.<br><br>"
 
-                         "The VEIL mobile app allows you to use your mobile device’s camera to scan QR codes, and send VEIL to the wallet that generated the QR code.<br><br>"
+                         "The Veil mobile app allows you to use your mobile device’s camera to scan QR codes, and send veil to the wallet that generated the QR code.<br><br>"
 
-                         "There are also many QR code scanning mobile apps that can scan a VEIL QR Code and show you the address as text. This text address can be manually copied to clipboard and pasted in the ”Enter receiving VEIL address” field of your wallet’s Send tab.");
+                         "There are also many QR code scanning mobile apps that can scan a Veil QR Code and show you the address as text. This text address can be manually copied to clipboard and pasted in the ”Enter receiving Veil address” field of your wallet’s Send tab.");
 
     ui->label_2->setTextFormat( Qt::RichText );
     ui->label_2->setWordWrap(true);
