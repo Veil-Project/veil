06b7c4884224a10e4099ded55f90b762b545ee9d
Strontium <38264865+StrontiumZ38@users.noreply.github.com>
2018-12-30 13:48:11 +0900

    Update settingsfaq10.cpp

--- src/qt/veil/settings/settingsfaq10.cpp
+++ src/qt/veil/settings/settingsfaq10.cpp
@@ -7,11 +7,11 @@ SettingsFaq10::SettingsFaq10(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->label->setText("The X16R mining algorithm will be used for at least the first 12 months of mainnet going live (December 8th, 2018) to achieve wide coin supply distribution.<br><br>"
+    ui->label->setText("The X16RT mining algorithm will be used for at least the first 12 months of mainnet going live (January 1st, 2019) to achieve wide coin supply distribution.<br><br>"
 
-                         "Anyone with NVIDIA or AMD graphics cards will be able to solo mine or pool mine VEIL without concerns about ASIC’s and mining centralization during the mining phase.<br><br>"
+                         "Anyone with NVIDIA or AMD graphics cards will be able to solo mine or pool mine veil without concerns about ASICs and mining centralization during the mining phase.<br><br>"
 
-                         "For the latest mining instructions, please see the Official VEIL Announcement thread on the <font color=#105aef>BitcoinTalk forum</font>.");
+                         "For the latest mining instructions, please see the Official Veil Announcement thread on the <font color=#105aef>BitcoinTalk forum</font>.");
 
     ui->label->setTextFormat( Qt::RichText );
     ui->label->setWordWrap(true);
