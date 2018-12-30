5033e7a0394e35cbf1b9a548b7b98d912fcdb72e
Strontium <38264865+StrontiumZ38@users.noreply.github.com>
2018-12-30 14:08:49 +0900

    Update settingsfaq11.cpp

--- src/qt/veil/settings/settingsfaq11.cpp
+++ src/qt/veil/settings/settingsfaq11.cpp
@@ -7,9 +7,9 @@ SettingsFaq11::SettingsFaq11(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->label_2->setText("Anyone can earn VEIL through the VEIL Bounty Program. A wide variety of tasks can be performed to earn VEIL, ranging from traditional bounty tasks, high reward contests, bug bounty, dev bounty, and adopting VEIL as a merchant or service.<br><br>"
+    ui->label_2->setText("Anyone can earn veil through the Veil Bounty Program. A wide variety of tasks can be performed to earn veil, ranging from traditional bounty tasks, high reward contests, bug bounty, dev bounty, and adopting Veil as a merchant or service.<br><br>"
 
-                         "For the latest information, please see the Officia VEIL Bounty Program thread on the <font color=#105aef>BitcoinTalk forum.</font>");
+                         "For the latest information, please see the Official Veil Bounty Program thread on the <font color=#105aef>BitcoinTalk forum.</font>");
 
     ui->label_2->setTextFormat( Qt::RichText );
     ui->label_2->setWordWrap(true);
