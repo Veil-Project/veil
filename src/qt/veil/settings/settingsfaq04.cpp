23df8005d902285825ae77134b833f5d0e96619a
Strontium <38264865+StrontiumZ38@users.noreply.github.com>
2018-12-30 15:15:41 +0900

    Update settingsfaq04.cpp

--- src/qt/veil/settings/settingsfaq04.cpp
+++ src/qt/veil/settings/settingsfaq04.cpp
@@ -7,9 +7,9 @@ SettingsFaq04::SettingsFaq04(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->label_2->setText("Newly received <font color=#105aef>veil requires ~6 minutes</font> to fully confirm and become mintable (and spendable if using the “”Single Non-Autominting Address or Exchange-or-Service” mode).<br><br>"
+    ui->label_2->setText("Newly received veil requires ~6 minutes to fully confirm and become mintable (and spendable if using the “”Single Non-Autominting Address or Exchange-or-Service” mode).<br><br>"
 
-                         "Newly minted <font color=#105aef>Zerocoin veil takes ~20 minutes</font> to become spendable. This delay is designed to protect users from ”timing attacks”, where their transactions are traceable by spending soon after minting.<br><br>"
+                         "Newly minted Zerocoin veil takes ~20 minutes to become spendable. This delay is designed to protect users from ”timing attacks”, where their transactions are traceable by spending soon after minting.<br><br>"
 
                          "Autominting happens automatically when your wallet is unlocked for Staking/Autominting and will slowly mint one of each of the available Zerocoin denominations every 6-7 blocks. It is possible to mint Zerocoin all in one block with Manual Minting, possibly with a higher fee. To access it, go to Settings>Zerocoin Minting and input how many Basecoin veil you want to mint into Zerocoin veil.");
 
