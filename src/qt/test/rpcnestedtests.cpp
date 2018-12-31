407d3af4b77704dd32bea094af5a75c58b3e1a3a
4x13 <39984029+4x13@users.noreply.github.com>
2018-12-31 14:59:18 -0800

    Update rpcnestedtests.cpp

--- src/qt/test/rpcnestedtests.cpp
+++ src/qt/test/rpcnestedtests.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2018 The veil Core developers
+// Copyright (c) 2018-2019 Veil developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
