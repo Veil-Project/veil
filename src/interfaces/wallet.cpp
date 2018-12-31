eb909fbd8fa8f1d82fbd9c6963eb1c2f1783b2ab
Anthony Posselli <anthony@paddington.services>
2018-12-31 16:40:18 -0700

    Begin work on choosing the right transaction type when sending through the GUI.

--- src/interfaces/wallet.cpp
+++ src/interfaces/wallet.cpp
@@ -46,6 +46,7 @@ public:
     {
         LOCK2(cs_main, m_wallet.cs_wallet);
         CValidationState state;
+
         if (!m_wallet.CommitTransaction(m_tx, std::move(value_map), std::move(order_form), m_key, g_connman.get(), state)) {
             reject_reason = state.GetRejectReason();
             return false;
@@ -373,19 +374,67 @@ public:
         LOCK2(cs_main, m_wallet.cs_wallet);
         return m_wallet.ListLockedCoins(outputs);
     }
-    std::unique_ptr<PendingWalletTx> createTransaction(const std::vector<CRecipient>& recipients,
+    std::unique_ptr<PendingWalletTx> createTransaction(std::vector<CTempRecipient>& recipients,
         const CCoinControl& coin_control,
         bool sign,
         int& change_pos,
         CAmount& fee,
+        OutputTypes inputType,
         std::string& fail_reason) override
     {
         LOCK2(cs_main, m_wallet.cs_wallet);
         auto pending = MakeUnique<PendingWalletTxImpl>(m_wallet);
-        if (!m_wallet.CreateTransaction(recipients, pending->m_tx, pending->m_key, fee, change_pos,
-                fail_reason, coin_control, sign)) {
+        size_t nRingSize = Params().DefaultRingSize();
+        size_t nInputsPerSig = 32;
+
+        CTransactionRef tx_new;
+        auto pwalletAnon = m_wallet.GetAnonWallet();
+
+        CWalletTx wtx(&m_wallet, tx_new);
+        CTransactionRecord rtx;
+
+        CAmount nFeeRet = 0;
+        bool fFailed = false;
+        bool fCheckFeeOnly = false;
+        switch (inputType) {
+            case OUTPUT_STANDARD:
+            {
+                if (0 !=
+                    pwalletAnon->AddStandardInputs(wtx, rtx, recipients, !fCheckFeeOnly, nFeeRet, &coin_control, fail_reason, false))
+                    fFailed = true;
+                break;
+            }
+            case OUTPUT_CT:
+                if (0 != pwalletAnon->AddBlindedInputs(wtx, rtx, recipients, !fCheckFeeOnly, nFeeRet, &coin_control, fail_reason))
+                    fFailed = true;
+                break;
+            case OUTPUT_RINGCT:
+                if (0 != pwalletAnon->AddAnonInputs(wtx, rtx, recipients, !fCheckFeeOnly, nRingSize, nInputsPerSig, nFeeRet, &coin_control, fail_reason))
+                    fFailed = true;
+                break;
+            default:
+                fFailed = true;
+        }
+
+        if (fFailed) {
+            fail_reason = "Failed to add inputs";
             return {};
         }
+
+        CValidationState state;
+        CReserveKey reservekey(&m_wallet);
+
+        pending->m_tx = tx_new;
+//        if (!m_wallet.CommitTransaction(wtx.tx, wtx.mapValue, wtx.vOrderForm, reservekey, g_connman.get(), state)) {
+//            fail_reason = "Transaction commit failed";
+//        }
+
+        fee = nFeeRet;
+
+//        if (!m_wallet.CreateTransaction(recipients, pending->m_tx, pending->m_key, fee, change_pos,
+//                fail_reason, coin_control, sign)) {
+//            return {};
+//        }
         return std::move(pending);
     }
     bool transactionCanBeAbandoned(const uint256& txid) override { return m_wallet.TransactionCanBeAbandoned(txid); }
@@ -560,6 +609,24 @@ public:
     CAmount getAvailableBalance(const CCoinControl& coin_control) override
     {
         return m_wallet.GetAvailableBalance(&coin_control);
+    }
+    CAmount getAvailableCTBalance(const CCoinControl& coin_control) override
+    {
+        if (!m_wallet.GetAnonWallet())
+            return 0;
+        return m_wallet.GetAnonWallet()->GetAvailableBlindBalance(&coin_control);
+    }
+    CAmount getAvailableRingCTBalance(const CCoinControl& coin_control) override
+    {
+        if (!m_wallet.GetAnonWallet()) {
+            std::cout << "Couldn't get anon wallet" << std::endl;
+            return 0;
+        }
+        return m_wallet.GetAnonWallet()->GetAvailableAnonBalance(&coin_control);
+    }
+    CWallet* getWalletPointer() override
+    {
+        return m_shared_wallet.get();
     }
     isminetype txinIsMine(const CTxIn& txin) override
     {
