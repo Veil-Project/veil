ac4a87a29ba0e022fce08ded2b0ce78e3a356681
4x13 <39984029+4x13@users.noreply.github.com>
2018-12-31 15:21:00 -0800

    Update outputrecord.cpp

--- src/veil/ringct/outputrecord.cpp
+++ src/veil/ringct/outputrecord.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The Veil Core developers
+// Copyright (c) 2018-2019 Veil developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
