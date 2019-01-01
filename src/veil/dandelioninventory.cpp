8bc85b5fdd0963e4b23c91617019f91449366613
4x13 <39984029+4x13@users.noreply.github.com>
2018-12-31 17:13:59 -0800

    Update copyrights part 1 (#130)
    * Update LICENSE
    
    * Update LICENSE
    
    * Update init.cpp
    
    * Update COPYING
    
    * Update rpcnestedtests.cpp
    
    * Update transactionrecord.cpp
    
    * Update bitcoinstrings.cpp
    
    * Updated dandelioninventory.cpp
    
    * Update temprecipient.cpp
    
    * Update transactionrecord.h
    
    * Update anonwalletdb.cpp
    
    * Update anonwallet.h
    
    * Update anon.cpp
    
    * Update anon.h
    
    * Update anonwallet.cpp
    
    * Update anonwalletdb.h
    
    * Update blind.cpp
    
    * Update blind.h
    
    * Update extkey.cpp
    
    * Update extkey.h
    
    * Update keyutil.cpp
    
    * Update keyutil.h
    
    * Update outputrecord.cpp
    
    * Update outputrecord.h
    
    * Update rctindex.h
    
    * Update rpcanonwallet.cpp
    
    * Update rpcanonwallet.h
    
    * Update stealth.cpp
    
    * Update stealth.h
    
    * Update temprecipient.h
    
    * Update types.h

--- src/veil/dandelioninventory.cpp
+++ src/veil/dandelioninventory.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The VEIL developers
+// Copyright (c) 2018-2019 Veil developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
--- src/veil/ringct/stealth.h
+++ src/veil/ringct/stealth.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2014 The ShadowCoin developers
-// Copyright (c) 2017 The Particl developers
-// Copyright (c) 2018 The Veil developers
+// Copyright (c) 2017-2019 The Particl developers
+// Copyright (c) 2018-2019 Veil developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
