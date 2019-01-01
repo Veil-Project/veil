7f725732fa2133c8c9300301298018eeb2662598
Strontium <38264865+StrontiumZ38@users.noreply.github.com>
2019-01-01 17:38:01 +0900

    Update mnemonicwalletinit.cpp

--- src/veil/mnemonic/mnemonicwalletinit.cpp
+++ src/veil/mnemonic/mnemonicwalletinit.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The VEIL developers
+// Copyright (c) 2019 The Veil developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
