35b60b50bc458336acb271b60147ca74701acc5c
Strontium <38264865+StrontiumZ38@users.noreply.github.com>
2019-01-01 18:50:36 +0900

    Update tx_verify.cpp

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -1,6 +1,6 @@
-// Copyright (c) 2017-2017 The Bitcoin Core developers
-// Copyright (c) 2015-2018 The PIVX developers
-// Copyright (c) 2018 The Veil developers
+// Copyright (c) 2017-2019 The Bitcoin Core developers
+// Copyright (c) 2015-2019 The PIVX developers
+// Copyright (c) 2019 The Veil developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
