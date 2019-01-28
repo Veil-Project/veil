c4f5f89a0b43609eee0a43da6f2dcfba300adbc8
presstab <presstab1337@gmail.com>
2019-01-28 11:33:05 -0700

    Mark zerocoins spent inside of AddToWalletIfInvolvingMe().

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -838,6 +838,7 @@ public:
     std::string GetUniqueWalletBackupName(bool fzAuto) const;
     bool UpdateMint(const CBigNum& bnValue, const int& nHeight, const uint256& txid, const libzerocoin::CoinDenomination& denom);
     void UpdateZerocoinState(const CMintMeta& meta);
+    void SetSerialSpent(const uint256& bnSerial, const uint256& txid);
     void ArchiveZerocoin(CMintMeta& meta);
     void AutoZeromint();
 
