72a62a9570702ba289e10ac2e61c968424118421
Mister K <43561981+MisterKangaroo@users.noreply.github.com>
2019-01-31 04:27:43 +0100

    text correction
    There was a typo "mind Basecoin into Zerocoin" instead of "mint..."

--- src/qt/veil/settings/settingsfaq08.cpp
+++ src/qt/veil/settings/settingsfaq08.cpp
@@ -13,7 +13,7 @@ SettingsFaq08::SettingsFaq08(QWidget *parent) :
 
                             "1. Send veil to the QR code or receiving address displayed in the overview tab to deposit veil to yourself.<br>"
                             "2. Click the Staking Toggle at the bottom left of the Overview screen and input your password to unlock your wallet for Staking/Autominting.<br>"
-                            "3. Allow the wallet to automatically mind your Basecoin veil into Zerocoin veil.<br>"
+                            "3. Allow the wallet to automatically mint your Basecoin veil into Zerocoin veil.<br>"
                             "4. After newly minted Zerocoin veil receives 200 confirmations, it will begin staking and can earn staking rewards. <br>"
                          "5. Ensure the staking icon at the bottom left of the Overview screen is turned on. Exiting your wallet will stop staking.");
 
