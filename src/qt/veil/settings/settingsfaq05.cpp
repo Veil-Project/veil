c76d04aa3386252ce5805ed72eeddc0c4ebf2d98
Mat Waller <mat@wallercrypto.com>
2019-07-19 12:59:53 +0100

    UI Updates for FAQ Content & Models.

--- src/qt/veil/settings/settingsfaq05.cpp
+++ src/qt/veil/settings/settingsfaq05.cpp
@@ -6,19 +6,6 @@ SettingsFaq05::SettingsFaq05(QWidget *parent) :
     ui(new Ui::SettingsFaq05)
 {
     ui->setupUi(this);
-
-    ui->label_2->setText("Zerocoin veil is used automatically when spending.<br><br>"
-
-                         "<font color=#105aef>To spend Basecoin veil, follow these instructions:</font><br><br>"
-
-                            "1. Navigate to the Send tab.<br>"
-                            "2. Click the Coin Control button.<br>"
-                            "3. Select all Basecoin UTXO inputs and send a transaction.<br><br>"
-
-                         "All Basecoin transactions are anonymized with RingCT. Although RingCT transactions have an anonymity set size of 11 versus Zerocoin’s thousands, it prevents Basecoin from compromising the privacy of Zerocoin transactions.");
-
-    ui->label_2->setTextFormat( Qt::RichText );
-    ui->label_2->setWordWrap(true);
 }
 
 SettingsFaq05::~SettingsFaq05()
--- src/qt/veil/settings/settingsfaq06.cpp
+++ src/qt/veil/settings/settingsfaq06.cpp
@@ -6,23 +6,6 @@ SettingsFaq06::SettingsFaq06(QWidget *parent) :
     ui(new Ui::SettingsFaq06)
 {
     ui->setupUi(this);
-
-    ui->label_2->setText("<font color=#105aef>Single Non-Autominting Address</font><br><br>"
-
-                         "The Single Non-Autominting Address allows users to avoid automatic minting of Basecoin veil into Zerocoin veil by keeping Basecoin veil inside it. To designate an address as their Single Non-Autominting Address, right-click/hold a transaction and convert it. The default wallet mode only allows one such address.<br><br>"
-
-                         "<font color=#105aef>Exchange or Service mode</font><br><br>"
-
-                         "Exchanges, merchants, and other services may want to avoid the Zerocoin autominting and/or staking process. By enabling Exchange or Service mode, the wallet will no longer automint or stake, with the advantage of only requiring ~6 minutes for received veil to become spendable again. It is highly discouraged for normal users to activate this mode due to the inability to stake and the relatively less anonymous RingCT transaction method.<br><br>"
-
-                         "<font color=#105aef>To change to Exchange or Service mode, follow these instructions:</font><br><br>"
-
-                            "1. Go to Settings>Open Configuration File<br>"
-                            "2. Enter the following as a new line in the veil.config file: exchangesandservicesmode=1<br>"
-                            "3. Save the file and restart the wallet	<br><br>");
-
-    ui->label_2->setTextFormat( Qt::RichText );
-    ui->label_2->setWordWrap(true);
 }
 
 SettingsFaq06::~SettingsFaq06()
