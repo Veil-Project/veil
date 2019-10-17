188150c2c90d2643f532f3cb4697aad480f00448
ja <jeremy.anderson.utah@gmail.com>
2019-10-17 17:18:23 -0600

    Integrate ProgPow

--- src/pow.h
+++ src/pow.h
@@ -15,10 +15,13 @@ class CBlockIndex;
 class uint256;
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&,
-                                    bool fProofOfStake);
-unsigned int DarkGravityWave(const CBlockIndex* pindexLast, const Consensus::Params& params, bool fProofOfStake);
+                                    bool fProofOfStake, bool fProgPow);
+unsigned int DarkGravityWave(const CBlockIndex* pindexLast, const Consensus::Params& params, bool fProofOfStake, bool fProgPow);
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&);
+
+/** Check whether a block hash satisfies the prog-proof-of-work requirement specified by nBits */
+bool CheckProgProofOfWork(const CBlockHeader& block, unsigned int nBits, const Consensus::Params&);
 
 #endif // BITCOIN_POW_H
