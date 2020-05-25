14f213c10fa6e94ec7c2cd73518bf9ecff971506
sinetek <pitwuu@gmail.com>
2020-05-25 15:29:56 +0200

    build: ensure we aren't using GNU extensions
    * add -Wgnu to compile flags
    * rename anonymous structs
    * remove variadic macros
    
    In the codebase we are making use of some GNU extensions. We should either remove their usage, or
    at least amend our CXX compiler checks.
    
      #### anonymous structs
      ```bash
      ./prevector.h:153:9: warning: anonymous structs are a GNU extension [-Wgnu-anonymous-struct]
              struct {
      ```
    
      #### variadic macros
    
      ```bash
      ./undo.h:57:50: warning: must specify at least one argument for '...' parameter of variadic macro [-Wgnu-zero-variadic-macro-arguments]
                  ::Unserialize(s, VARINT(nVersionDummy));
      ```
      #### prevention
      To ensure that usage doesn't creep back in we can add [`-Wgnu`](https://clang.llvm.org/docs/DiagnosticsReference.html#wgnu) to our compile time flags, which will make Clang warn whenever it encounters GNU extensions.

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -442,7 +442,7 @@ public:
                 ::Unserialize(s, CTxOutCompressor(vout[i]));
         }
         // coinbase height
-        ::Unserialize(s, VARINT(nHeight, VarIntMode::NONNEGATIVE_SIGNED));
+        ::Unserialize(s, VARINT_MODE(nHeight, VarIntMode::NONNEGATIVE_SIGNED));
     }
 };
 
