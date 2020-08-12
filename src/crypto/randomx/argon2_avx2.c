798318777f1fed81021deeae1dbf70f29579744e
Cave Spectre <crypto@pontificatingnobody.com>
2020-08-11 21:58:09 -0400

    [Build] Update for ARM builds

--- src/crypto/randomx/argon2_avx2.c
+++ src/crypto/randomx/argon2_avx2.c
@@ -41,6 +41,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 void randomx_argon2_fill_segment_avx2(const argon2_instance_t* instance,
 	argon2_position_t position);
 
+#if defined(__AVX2__)
 randomx_argon2_impl* randomx_argon2_impl_avx2() {
 
 #if defined(__AVX2__)
@@ -48,6 +49,7 @@ randomx_argon2_impl* randomx_argon2_impl_avx2() {
 #endif
 	return NULL;
 }
+#endif
 
 #if defined(__AVX2__)
 #include <immintrin.h>
